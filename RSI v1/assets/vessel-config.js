91c580b32fc4737e22f4d55665674fb1048f72ac
aerith2795 <the.hexpresso.interface@gmail.com>
2025-09-26 18:08:49 +0800

    big update

---
+++ RSI v1/assets/vessel-config.js
@@ -0,0 +1,11 @@
+export default {
+  "welcomeMessage": "Welcome aboard M/V Yurisan Dozo! Please review the vessel information and complete your onboarding steps.",
+  "aboutVessel": "M/V Yurisan Dozo is a state-of-the-art vessel built in 2023, designed for long voyages. It features modern navigation systems, eco-friendly propulsion, and accommodates 200 crew members.",
+  "photos": [
+    "../assets/vessel1.jpg",
+    "../assets/vessel2.jpg",
+    "../assets/vessel3.jpg",
+    "../assets/vessel4.jpg"
+  ],
+  "modules": ["Embarkation", "Certificates", "GDPR", "Video"]
+};
